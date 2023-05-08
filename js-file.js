c65245abb239bad6690be80f088c1609d1cc1d1c
Nikola Ruthless <105891852+aerondight997@users.noreply.github.com>
2023-05-08 23:39:00 +0200

    Added conditionals with working logic

--- js-file.js
+++ js-file.js
@@ -1,16 +1,39 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
+
+let winningMessage = "";
 
 function getComputerChoice() {
     randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
 
-function playRound(playerChoice, computerChoice) {
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
     
+     if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        
+           winningMessage = "You won! ${playerSelection} beats ${computerSelection}";
 
+         }
+     if ((computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "paper" && playerSelection == "rock") ||
+        (computerSelection == "scissors" && playerSelection == "paper")) {
+
+           winningMessage = "You lost! ${computerSelection} beats ${playerSelection}";
+
+         }
+     if (playerSelection == computerSelection) {
+
+        winningMessage = "It's tied! Try again!";
+     }
+  return winningMessage;        
 }
 
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerSelection));
 
-const playerChoice = "Rock";
-const computerChoice = getComputerChoice();
-console.log(playRound(playerChoice, computerChoice));
+
+
