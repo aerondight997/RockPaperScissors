81e26298b88507fdc76df230bc311020c35589eb
Nikola Ruthless <105891852+aerondight997@users.noreply.github.com>
2023-05-08 20:06:24 +0200

    Added first set of conditionals in playRound

--- js-file.js
+++ js-file.js
@@ -1,9 +1,15 @@
+const choices = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
     randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
 
 function playRound(playerChoice, computerChoice) {
-
+    if (playerChoice.toLowerCase() === "Rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock."
+    }
+    else if (playerChoice.toLowerCase() === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock."
+    }  
 }
