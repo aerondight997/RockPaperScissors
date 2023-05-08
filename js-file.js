64a7fd74afc960b5338e109d99fdc61798069320
Nikola Ruthless <105891852+aerondight997@users.noreply.github.com>
2023-05-08 19:26:11 +0200

    Added choices array

--- js-file.js
+++ js-file.js
@@ -1,3 +1,5 @@
 function getComputerChoice {
-    
+    const choices = ["Rock", "Paper", "Scissors"];
+    randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
 }
