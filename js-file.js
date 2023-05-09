683bfc9f9abb4b631254fb415833c98f0f91a8d0
Nikola Ruthless <105891852+aerondight997@users.noreply.github.com>
2023-05-09 15:44:41 +0200

    Added if statements for winner variable

--- js-file.js
+++ js-file.js
@@ -37,21 +37,41 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     
     let userInput = prompt();
-    let scoreCount;
+    let scoreCount = 0;
     let winner = '';
-    let scorePlayer;
-    let scoreComputer;
+    let scorePlayer = 0;
+    let scoreComputer = 0;
 
     for(scoreCount = 0; scoreCount <= 5; scoreCount++) {
-        playRound(playerSelection, computerSelection) 
+        playRound(playerSelection, computerSelection); 
         
         if(winningMessage = `You won! ${playerSelection} beats ${computerSelection}.`) {
-               scorePlayer++
-        } else if (winningMessage = `You won! ${computerSelection} beats ${playerSelection}.`) {
-               scoreComputer++;
-        }
+               
+               scorePlayer++;
 
+        } else if 
+          (winningMessage = `You won! ${computerSelection} beats ${playerSelection}.`) {
+               
+               scoreComputer++;
+
+        } else if 
+          (winningMessage = 'Its tied! Try again!') {
+            
+            playRound(playerSelection, computerSelection); 
+            
+        }
     }
+
+    if (scorePlayer === 5){
+        
+        winner = "Human is victorious! Computer is done for!";
+
+    } else if 
+       (scoreComputer === 5) {
+
+         winner = "Computer has won! Human runs in fear!";
+
+       }
 }
 
 
