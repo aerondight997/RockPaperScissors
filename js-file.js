cef969c9c598c858b0fd250e0fe3f1921f5b3e75
Nikola Ruthless <105891852+aerondight997@users.noreply.github.com>
2023-05-10 20:43:11 +0200

    Added console logs and lowercase function to prompt

--- js-file.js
+++ js-file.js
@@ -1,9 +1,9 @@
 const choices = ['rock', 'paper', 'scissors'];
+
 let playerScore = 0;
 let computerScore = 0;
 let tieCounter = 0;
-let matchResult = 0;
-
+let matchResult = "";
 
 function getComputerChoice() {
     
@@ -16,8 +16,8 @@ function playRound(playerSelection, computerSelection) {
       if ((playerSelection == "rock" && computerSelection == "scissors") ||
           (playerSelection == "paper" && computerSelection == "rock") || 
           (playerSelection == "scissors" && computerSelection == "paper")) {
-        
-          return playerScore++;
+          
+             return playerScore++;
            
          } else if 
          
@@ -25,7 +25,7 @@ function playRound(playerSelection, computerSelection) {
           (computerSelection == "paper" && playerSelection == "rock") ||
           (computerSelection == "scissors" && playerSelection == "paper")) {
 
-            return computerScore++;
+             return computerScore++;
 
          } else if 
          
@@ -33,32 +33,44 @@ function playRound(playerSelection, computerSelection) {
           (computerSelection == "paper" && playerSelection == "paper") ||
           (computerSelection == "scissors" && playerSelection == "scissors")) {
             
-            return tieCounter++;
+             return tieCounter++;
 
           }
 }  
 
 
 function game() {
-  
+
   for(let rounds = 0; rounds < 5; rounds++) {
-    const playerSelection = prompt("Which sign do you choose?");
+
+    const playerSelection = prompt("Which sign do you choose?").toLowerCase();
     const computerSelection = getComputerChoice();  
+    
     let roundResult = playRound(playerSelection, computerSelection);
+    
+    console.log(`Player result: ${playerScore}`);
+    console.log(`Computer result: ${computerScore}`);
+    console.log(`Tie counts: ${tieCounter}`);
   }
   
+  
+
   if (playerScore > computerScore) {
      
      matchResult = "You won! Player beats the computer!";
 
-  } else if
-     (computerScore > playerScore) {
+  } else if (computerScore > playerScore) {
 
-     matchResult = "Computer wins! You lost!" 
+     matchResult = "Computer wins! You lost!"; 
 
-     }
+  } else if (computerScore == playerScore) {
+
+     matchResult = "You are tied! Both of you SUCK!";
+
+  } 
  return matchResult;        
 }
+
 
 console.log(game());
 
